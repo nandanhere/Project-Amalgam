41858254c2b5d289f973eb91a20bedeb9bd138d5
TheNova22 <jayantsogikar@gmail.com>
2021-03-20 13:57:54 +0530

    Settings Page UI Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_amalgam/screens/Auth_Page/auth_page.dart';
+import 'package:project_amalgam/screens/Settings_Page/settings_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Amalgam',
+        routes: {
+          SettingsPage.routeName: (context) => SettingsPage(),
+        },
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: AuthScreen());
+        home: SettingsPage());
   }
 }
